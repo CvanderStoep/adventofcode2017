d923fa0414afa2b66aea508aaeb76ecbc803897c
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2025-03-23 12:32:28 +0100

    day8 finished

--- day8.py
+++ day8.py
@@ -1,5 +1,5 @@
-import operator
 from collections import defaultdict
+
 
 def read_input_file(file_name: str) -> list:
     try:
@@ -13,44 +13,34 @@ def read_input_file(file_name: str) -> list:
         print(f"Error: Could not read the file '{file_name}'.")
         return []
 
+
 def process_instruction(registers: dict, instruction: str, max_value: int) -> tuple[dict, int]:
-    # Map operators to their functions
-    ops = {
-        '==': operator.eq,
-        '!=': operator.ne,
-        '>': operator.gt,
-        '<': operator.lt,
-        '>=': operator.ge,
-        '<=': operator.le
-    }
-
-    register1, modify, digit1, _, register2, operator_symbol, digit2 = instruction.split()
-    condition_met = ops[operator_symbol](registers[register2], int(digit2))
-
+    register1, modify, digit1, _, register2, operator, digit2 = instruction.split()
+    register_value = registers[register2]
+    condition_met = eval(str(register_value) + operator + digit2)
     if condition_met:
         if modify == 'inc':
             registers[register1] += int(digit1)
-        elif modify == 'dec':
+        else:
             registers[register1] -= int(digit1)
 
-    max_value = max(max_value, max(registers.values(), default=0))
+    max_value = max(max_value, max(registers.values()) )
     return registers, max_value
 
 def compute_part(file_name: str) -> None:
     registers = defaultdict(int)
 
     instructions = read_input_file(file_name)
-    if not instructions:
-        return  # Exit if no instructions were read
-
     max_value = 0
     for instruction in instructions:
         registers, max_value = process_instruction(registers, instruction, max_value)
 
-    max_register_value = max(registers.values(), default=0)
+    max_register_value = max(registers.values())
 
-    print(f'Part I: The maximum value of any register after all operations is: {max_register_value}')
-    print(f'Part II: The highest value held in any register during operations is: {max_value}')
+    print(f'partI: The maximum value of the register after all operations is: {max_register_value}')
+    print(f'partII: The maximum value during the operations: {max_value}')
+
+
 
 if __name__ == '__main__':
     file_path = 'input/input8.txt'
